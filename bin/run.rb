d9c9d285c48172fc29f8d66809722e6b0e118925
kzkaed <kzkaeding@gmail.com>
2014-12-28 14:42:51 -0600

    refactored comments out

--- bin/run.rb
+++ bin/run.rb
@@ -4,7 +4,6 @@ require_relative "../lib/mastermind/console"
 
 puts "Hello, I'm starting the game..."
 game = Mastermind::Game.new
-#user_io = Mastermind::UserIO.new
 console = Mastermind::Console.new
 console.play(game)
 
